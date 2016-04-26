bb8f31b98ca283bcba227e66b2a20bc509eb7ed3
jwz2kn <jwz2kn@virginia.edu>
2016-04-25 23:36:30 -0400

    Limits on text fields

--- BookSwap/SellInfoViewController.swift
+++ BookSwap/SellInfoViewController.swift
@@ -134,7 +134,81 @@ class SellInfoViewController: UIViewController, UIImagePickerControllerDelegate,
         }
     }
 
- 
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String)-> Bool {
+        // We ignore any change that doesn't add characters to the text field.
+        // These changes are things like character deletions and cuts, as well
+        // as moving the insertion point.
+        //
+        // We still return true to allow the change to take place.
+        if string.characters.count == 0 {
+            return true
+        }
+        
+        // Check to see if the text field's contents still fit the constraints
+        // with the new content added to it.
+        // If the contents still fit the constraints, allow the change
+        // by returning true; otherwise disallow the change by returning false.
+        let currentText = textField.text ?? ""
+        let prospectiveText = (currentText as NSString).stringByReplacingCharactersInRange(range, withString: string)
+        
+        switch textField {
+            
+            // Allow only upper- and lower-case vowels in this field,
+        // and limit its contents to a maximum of 6 characters.
+        case ISBNField:
+            return containsOnlyCharactersIn(prospectiveText, matchCharacters: "0123456789") &&
+                prospectiveText.characters.count <= 13
+            
+//            // Allow any characters EXCEPT upper- and lower-case vowels in this field,
+//        // and limit its contents to a maximum of 8 characters.
+//        case noVowelsTextField:
+//            return prospectiveText.doesNotContainCharactersIn("aeiouAEIOU") &&
+//                prospectiveText.characters.count <= 8
+//            
+//            // Allow only digits in this field,
+//        // and limit its contents to a maximum of 3 characters.
+//        case digitsOnlyTextField:
+//            return prospectiveText.containsOnlyCharactersIn("0123456789") &&
+//                prospectiveText.characters.count <= 3
+//            
+//            // Allow only values that evaluate to proper numeric values in this field,
+//        // and limit its contents to a maximum of 7 characters.
+        case PriceField:
+            return isNumeric(prospectiveText) && doesNotContainCharactersIn(prospectiveText, matchCharacters: "-e") &&
+                prospectiveText.characters.count <= 10
+            
+            // In this field, allow only values that evalulate to proper numeric values and
+            // do not contain the "-" and "e" characters, nor the decimal separator character
+        // for the current locale. Limit its contents to a maximum of 5 characters.
+//        case positiveIntegersOnlyTextField:
+//            let decimalSeparator = NSLocale.currentLocale().objectForKey(NSLocaleDecimalSeparator) as! String
+//            return prospectiveText.isNumeric() &&
+//                prospectiveText.doesNotContainCharactersIn("-e" + decimalSeparator) &&
+//                prospectiveText.characters.count <= 5
+            
+        // Do not put constraints on any other text field in this view
+        // that uses this class as its delegate.
+        default:
+            return true
+        }
+        
+    }
+    
+    func containsOnlyCharactersIn(prospText: String, matchCharacters: String) -> Bool {
+        let disallowedCharacterSet = NSCharacterSet(charactersInString: matchCharacters).invertedSet
+        return prospText.rangeOfCharacterFromSet(disallowedCharacterSet) == nil
+    }
+    
+    func doesNotContainCharactersIn(prospText: String, matchCharacters: String) -> Bool {
+        let characterSet = NSCharacterSet(charactersInString: matchCharacters)
+        return prospText.rangeOfCharacterFromSet(characterSet) == nil
+    }
+    
+    func isNumeric(prospectiveText: String) -> Bool {
+        let scanner = NSScanner(string: prospectiveText)
+        scanner.locale = NSLocale.currentLocale()
+        return scanner.scanDecimal(nil) && scanner.atEnd
+    }
     
     // MARK: UIImagePickerControllerDelegate Methods
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
